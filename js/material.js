8f318fd21b5d09eb6c94347d5b61e8f9aa211c39
= <ayxect34525599@gmail.com>
2020-10-02 14:15:13 +0900

    set onclick at material

--- js/material.js
+++ js/material.js
@@ -27,5 +27,58 @@ function showStagesToGet(jsonData) {
     document.getElementById("description").innerHTML = message;
 }
 
+// 上位素材を表示する
+function showSuperiorMaterial(jsonData) {
+    let message= "";
+    let materials = jsonData["materials"][0]["superiorMaterial"]
+
+    if (materials) {
+        for (const material of materials) {
+            message = message + `<p>${material}</p>`;
+        }
+    } else {
+        message = "<p>上位素材はありません。</p>"
+    }
+
+    document.getElementById("description").innerHTML = message;
+}
+
+// 下位素材を表示する
+function showLowMaterial(jsonData) {
+    let message= "";
+    let materials = jsonData["materials"][0]["lowMaterial"]
+
+    if (materials) {
+        for (const material of materials) {
+            message = message + `<p>${material}</p>`;
+        }
+    } else {
+        message = "<p>下位素材はありません。</p>"
+    }
+
+    document.getElementById("description").innerHTML = message;
+}
+
 // html読み込み時、収集場所をデフォルトとして表示
 window.addEventListener('DOMContentLoaded', initJson("../src/json/M_Material.json"));
+
+// "収集場所"クリック時、収集場所を表示
+document.getElementById("stageToGet").onclick = function() {
+    fetch("../src/json/M_Material.json")
+        .then(response => response.json())
+        .then(data => showStagesToGet(data))
+}
+
+// "上位素材"クリック時、上位素材を表示
+document.getElementById("superiorMaterial").onclick = function() {
+    fetch("../src/json/M_Material.json")
+        .then(response => response.json())
+        .then(data => showSuperiorMaterial(data))
+}
+
+// "下位素材"クリック時、下位素材を表示
+document.getElementById("lowMaterial").onclick = function() {
+    fetch("../src/json/M_Material.json")
+        .then(response => response.json())
+        .then(data => showLowMaterial(data))
+}
