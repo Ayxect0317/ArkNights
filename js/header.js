4b3fa9fe5525753352882218b5f9f535932ce3f4
= <ayxect34525599@gmail.com>
2020-12-02 18:31:57 +0900

    add hamburger button

--- js/header.js
+++ js/header.js
@@ -6,13 +6,30 @@ import recalcNowTime from "../js/util/showNowTime.js";
 async function createHeader() {
   const response = await fetch("../html/header.html");
   const doc = new DOMParser().parseFromString(await response.text(), 'text/html');
-  // docはHTMLObjectなので、outerHTMLで文字列に変換
-  const header = doc.querySelector("nav").outerHTML;
-  document.querySelector("header").innerHTML = header;
+  // docはHTMLObjectなので、文字列に変換
+  document.querySelector("header").innerHTML = doc.querySelector("body").innerHTML;
 
   // header.jsのDOM構造の読み込みが完了した後動作させる
   // 単に recalcNowTime() と書くと、header.htmlのDOM構造が読み込まれる前に実行されてしまう
   window.addEventListener("DOMContentLoaded", recalcNowTime());
+
+  return new Promise(resolve => {
+    resolve("ok");
+  })
 }
 
-createHeader();
+function toggleNav() {
+  const hamburgerMenu = document.querySelectorAll(".hamburgerMenu");
+  hamburgerMenu.forEach(target => {
+    target.addEventListener("click", () => {
+      document.getElementsByClassName("hamburger")[0].classList.toggle("open");
+    });
+  })
+}
+
+async function exec() {
+  let promise = await createHeader();
+  toggleNav();
+}
+
+exec();
