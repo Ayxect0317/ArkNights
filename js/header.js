dfabeef145cb9e7dfd39bb5fc9a90edf0e3516f4
= <ayxect34525599@gmail.com>
2020-12-03 18:25:27 +0900

    upgrade hamburger menu

--- js/header.js
+++ js/header.js
@@ -9,22 +9,31 @@ async function createHeader() {
   // docはHTMLObjectなので、文字列に変換
   document.querySelector("header").innerHTML = doc.querySelector("body").innerHTML;
 
-  // header.jsのDOM構造の読み込みが完了した後動作させる
+  // header.jsのDOM構造の読み込みが完了した後に動作させる
   // 単に recalcNowTime() と書くと、header.htmlのDOM構造が読み込まれる前に実行されてしまう
   window.addEventListener("DOMContentLoaded", recalcNowTime());
 
-  return new Promise(resolve => {
-    resolve("ok");
-  })
+  return new Promise(resolve => { resolve(); })
 }
 
 function toggleNav() {
   const hamburgerMenu = document.querySelectorAll(".hamburgerMenu");
+  const hamburger = document.getElementsByClassName("hamburger")[0];
+  const blackBg = document.getElementsByClassName("blackBg")[0];
+
+  // ハンバーガーメニューをクリックしたとき、ナビゲーションがトグルする
   hamburgerMenu.forEach(target => {
     target.addEventListener("click", () => {
-      document.getElementsByClassName("hamburger")[0].classList.toggle("open");
+      hamburger.classList.toggle("opened__hamburger");
+      blackBg.classList.toggle("opened__hlackBg");
     });
-  })
+  });
+
+  // 黒い背景をクリックしたとき、ナビゲーションが消える
+  blackBg.addEventListener("click", () => {
+    hamburger.classList.remove("opened__hamburger");
+    blackBg.classList.remove("opened__hlackBg");
+  });
 }
 
 async function exec() {
